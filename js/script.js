401a9f9e3c9c1a4dcd9e36e734fca9fd30c9f1b9
Alexey Muravitsky <rosenammajunas@gmail.com>
2024-07-19 19:36:37 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menuIcon");
   const headerNav = document.getElementById("headerNav");
+  const navLinks = headerNav.querySelectorAll("a");
 
   menuIcon.addEventListener("click", function () {
     headerNav.classList.toggle("active");
     document.body.classList.toggle("no-scroll");
+    headerNav.classList.toggle("open");
+    menuIcon.classList.toggle("burger-active");
+  });
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      headerNav.classList.remove("active");
+      document.body.classList.remove("no-scroll");
+      headerNav.classList.remove("open");
+      menuIcon.classList.remove("burger-active");
+    });
   });
 });
