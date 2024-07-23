4eeccce2743835c6a4cf6ac433b2581645b88ecd
Alexey Muravitsky <rosenammajunas@gmail.com>
2024-07-23 03:45:01 +0300

    refactor: apply mixed improvements from code review

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Демонстрация: https://alexeymuravitsky.github.io/RoomTibet/
+
+Макет: https://www.figma.com/design/NrPZZU8u1sLytOicIHcIAx/%D0%A0%D1%83%D0%BC%D0%A2%D0%B8%D0%B1%D0%B5%D1%82?node-id=0-1&t=RRbJK40OSIKyrW8u-0
+
+Валидатор html и css без ошибок: https://validator.w3.org/
+
+Проверил секции на переполнение
+
+Результаты Lighthouse:
+
+- [desktop](./img/lighthouse-desktop.webp)
+- [mobile](./img/lighthouse-mobile.webp)
--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const menuIcon = document.getElementById("menuIcon");
   const headerNav = document.getElementById("headerNav");
   const navLinks = headerNav.querySelectorAll("a");
 
-  menuIcon.addEventListener("click", function () {
+  menuIcon.addEventListener("click", () => {
     headerNav.classList.toggle("active");
     document.body.classList.toggle("no-scroll");
     headerNav.classList.toggle("open");
     menuIcon.classList.toggle("burger-active");
   });
 
-  navLinks.forEach(function (link) {
-    link.addEventListener("click", function () {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
       headerNav.classList.remove("active");
       document.body.classList.remove("no-scroll");
       headerNav.classList.remove("open");
       menuIcon.classList.remove("burger-active");
+
+      const targetSection = document.querySelector(
+        `[data-section="${link.getAttribute("href").substring(1)}"]`
+      );
+
+      targetSection.scrollIntoView({ behavior: "smooth" });
     });
   });
 });
